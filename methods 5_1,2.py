1384afa96a143fa507dab0a53e6064ae248f1db1
terbij-47 <varennikova.mv@gmail.com>
2024-11-04 22:49:20 +0300

    Update and rename methods 5_1.py to methods 5_1,2.py
    Methods __len__() and __str__() were added

--- methods 5_1,2.py
+++ methods 5_1,2.py
@@ -9,8 +9,20 @@ class House:
         else:
             for i in range(new_floor):
                 print(i + 1)
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
